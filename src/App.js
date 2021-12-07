70c11465b68de1a09743dc186da29b9d9a5199a0
Tudor Tocan <tudor.tocan@gmail.com>
2021-12-07 04:11:12 +0000

    add code to render table only on click of the button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import React, { useState } from "react";
+
 import "./App.css";
 import LandingPage from "./pages/landing-page";
 
 const App = () => {
+  const [showData, setShowData] = useState(false);
+
   return (
     <div className="App">
       <h1>
         <i className="fa fa-circle text-danger Blink"></i>&nbsp; MST Systems -
-        Live Updates{" "}
+        Live Data Table
       </h1>
-      <LandingPage />
+      {!showData && (
+        <button
+          className="show-table-button"
+          onClick={() => {
+            setShowData(true);
+          }}
+        >
+          Show Table
+        </button>
+      )}
+      {showData && <LandingPage />}
     </div>
   );
 };
