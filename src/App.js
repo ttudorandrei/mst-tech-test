3351d7cf67ea2162cf2168fc55c3cd3d93bee404
Tudor Tocan <tudor.tocan@gmail.com>
2021-12-07 03:47:04 +0000

    add styled table and header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import LandingPage from "./pages/landing-page";
 const App = () => {
   return (
     <div className="App">
+      <h1>
+        <i className="fa fa-circle text-danger Blink"></i>&nbsp; MST Systems -
+        Live Updates{" "}
+      </h1>
       <LandingPage />
     </div>
   );
